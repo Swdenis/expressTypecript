db0d64666e855ee9b5f1b249e494a96a5eb41e2b
Swdenis <swdenis@mail.ru>
2021-04-26 17:05:53 +0200

    Refactor with descriptors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from 'express'
-import { router } from './routes/loginRoutes'
 import bodyParser from 'body-parser'
 import cookieSession from 'cookie-session'
+import './controllers/LoginController'
+import './controllers/RootController'
+import { AppRouter } from './AppRouter'
 
 const PORT = 3000
 
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(cookieSession({ name: 'session', keys: ['nndioas'] }))
-app.use(router)
+app.use(cookieSession({keys: ['nndioas'] }))
+app.use(AppRouter.getInstance())
 
 app.listen(PORT,()=>{console.log(`Listening on port ${PORT}`)})
